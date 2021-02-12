c4b2fbefdb9013d511be726a71915da3c71675a0
soderlpo <soderlpo@arcada.fi>
2021-02-12 10:56:46 +0200

    added fetch.php

---
+++ Back-end2021pr2/fetch.php
@@ -0,0 +1,13 @@
+<?php
+// Koppla oss till databasen
+$conn = create_conn();
+// Skapa SQL kommando
+$sql = "SELECT * FROM users";
+// Kör SQL kommando på databasen
+$result = $conn->query($sql);
+
+//Skapa en while loop för att hämta varje rad
+while ($row = $result->fetch_assoc()) {
+// Skriv ut endast ett värde (en kolumn och en rad -- en cell)
+print("Användare i databasen: " . $row['username'] . "</br>");
+}
